3c3a530b0ce318eefd7c1ddcb2765e005d8d4d5e
AFRO <124509829+AFRO09@users.noreply.github.com>
2024-02-25 11:05:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["🧡 Invision Official Host"];
 
 
 let currentIndex = 0;
